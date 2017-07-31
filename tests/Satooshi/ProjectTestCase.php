4e52be7703c9a46223d22f4b56c7d0671347e1e5
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2017-07-31 23:43:55 +0200

    ConfiguratorTest::assertConfiguration - normalize path in one place

--- tests/Satooshi/ProjectTestCase.php
+++ tests/Satooshi/ProjectTestCase.php
@@ -70,4 +70,26 @@ class ProjectTestCase extends \PHPUnit_Framework_TestCase
             rmdir($dir);
         }
     }
+
+    protected function normalizePath($path)
+    {
+        return strtr(DIRECTORY_SEPARATOR, '/', $path);
+    }
+
+    protected function assertSamePath($expected, $input, $msg = null)
+    {
+        $this->assertSame(
+            $this->normalizePath($expected),
+            $this->normalizePath($input),
+            $msg
+        );
+    }
+
+    protected function assertSamePaths(array $expected, array $input, $msg = null)
+    {
+        $expected = array_map(function ($path) { return $this->normalizePath($path); }, $expected);
+        $input = array_map(function ($path) { return $this->normalizePath($path); }, $input);
+
+        $this->assertSame($expected, $input, $msg);
+    }
 }
